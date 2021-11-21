3b93626e05eef7ccdfe6003dc1bfe3846509350a
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2021-11-21 17:31:32 +0530

    files added

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -5,14 +5,17 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.scss'],
+  styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
-  constructor(private router: Router) {}
 
-  ngOnInit(): void {}
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+  }
 
   onSubmit(form: NgForm) {
     this.router.navigate(['search', form.value.search]);
   }
+
 }
